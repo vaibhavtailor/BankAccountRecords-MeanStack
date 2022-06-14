187aaf7360c8cc0b1ae8968084ddc7b13715c295
vaibhavtailor <200458209@student.georgianc.on.ca>
2022-06-14 11:46:38 -0400

    Setup Angular Material UI Library

--- angular-mean-stack-bank-records/src/app/app.module.ts
+++ angular-mean-stack-bank-records/src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AngularMaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 import { AddAccountComponent } from './components/add-account/add-account.component';
 import { EditAccountComponent } from './components/edit-account/edit-account.component';
 import { AccountsListComponent } from './components/accounts-list/accounts-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,12 @@ import { AccountsListComponent } from './components/accounts-list/accounts-list.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
