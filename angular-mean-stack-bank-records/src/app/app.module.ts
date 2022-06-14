4b43a1d34c33497058657fbb62532afdf82a2015
vaibhavtailor <200458209@student.georgianc.on.ca>
2022-06-14 11:34:26 -0400

    Added components

--- angular-mean-stack-bank-records/src/app/app.module.ts
+++ angular-mean-stack-bank-records/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddAccountComponent } from './components/add-account/add-account.component';
+import { EditAccountComponent } from './components/edit-account/edit-account.component';
+import { AccountsListComponent } from './components/accounts-list/accounts-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddAccountComponent,
+    EditAccountComponent,
+    AccountsListComponent
   ],
   imports: [
     BrowserModule,
