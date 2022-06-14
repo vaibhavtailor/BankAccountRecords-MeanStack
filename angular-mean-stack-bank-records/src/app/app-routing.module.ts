93f4be007adc8eb30073e735a709c1603290b681
vaibhavtailor <200458209@student.georgianc.on.ca>
2022-06-14 11:38:30 -0400

    Setting up Routes to navigate between components.

--- angular-mean-stack-bank-records/src/app/app-routing.module.ts
+++ angular-mean-stack-bank-records/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddAccountComponent } from './components/add-account/add-account.component';
+import { EditAccountComponent } from './components/edit-account/edit-account.component';
+import { AccountsListComponent } from './components/accounts-list/accounts-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'add-account'},
+  { path: 'add-account', component: AddAccountComponent},
+  { path: 'edit-account', component: EditAccountComponent},
+  { path: 'accounts-list', component: AccountsListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
